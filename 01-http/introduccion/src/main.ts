ac598753375142f6e66dce0ad81acb4a859a9066
anderson Lucero <andersonlucero@hotmail.com>
2020-07-03 13:37:30 -0500

    creacion de archivos http modules

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //Aqui configuracion antes del app.listen()
+  //await app.listen(3000)
+  
+  await app.listen(3001);
 }
 bootstrap();
